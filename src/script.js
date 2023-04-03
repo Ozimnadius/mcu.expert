b574d1c4be4f2e0ba787baa7c19aaa94490665d7
Mikle Seregin <mikhail-seregin@yandex.ru>
2023-04-03 21:40:58 +0300

    update

--- src/script.js
+++ src/script.js
@@ -1,2 +1,27 @@
+window.addEventListener('DOMContentLoaded', function () {
+    init();
+});
 
+function init() {
+    initSliders();
+}
+
+function initSliders() {
+    const indexBannerSlider = document.querySelector('.index-banner__slider');
+    if (indexBannerSlider) {
+        const indexBannerSwiper = new Swiper(indexBannerSlider.querySelector('.index-banner__swiper'), {
+            grabCursor: true,
+            // If we need pagination
+            pagination: {
+                el: indexBannerSlider.querySelector('.index-banner__pag'),
+                clickable: true
+            },
+            // Navigation arrows
+            navigation: {
+                nextEl: indexBannerSlider.querySelector('.index-banner__next'),
+                prevEl: indexBannerSlider.querySelector('.index-banner__prev'),
+            },
+        });
+    }
+}
 
